f54d1cd4bced5dc5139ac4fc179d529a6639b739
Maddi <maddi.fitzmaurice@gmail.com>
2023-05-19 13:24:27 +1000

    Login Page To Score Entry Page
    - Login page now takes you to score entry page

--- ScoreEntry/LoginForm.cs
+++ ScoreEntry/LoginForm.cs
@@ -16,5 +16,12 @@ namespace ScoreEntry
         {
             InitializeComponent();
         }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            ScoreEntryForm scoreEntryForm = new ScoreEntryForm();
+            scoreEntryForm.Show();
+            this.Hide();
+        }
     }
 }
